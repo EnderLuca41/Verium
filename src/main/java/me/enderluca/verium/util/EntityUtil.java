2771753bedbc29eb75d94c557f6d2690c11d745e
EnderLuca41 <lucavogel111@gmail.com>
2023-11-05 23:47:15 +0100

    Added methods to create the firework when all goals are completed

--- src/main/java/me/enderluca/verium/util/EntityUtil.java
+++ src/main/java/me/enderluca/verium/util/EntityUtil.java
@@ -1,9 +1,14 @@
 package me.enderluca.verium.util;
 
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Wolf;
+import org.bukkit.inventory.meta.FireworkMeta;
 
 public final class EntityUtil {
     private EntityUtil(){}
@@ -17,5 +22,37 @@ public final class EntityUtil {
         wolf.setCustomName(name);
         wolf.setCustomNameVisible(true);
         return wolf;
+    }
+
+    private static Firework createVictoryFirework(World world, Location location, Color color, FireworkEffect.Type type){
+        Firework firework = world.spawn(location, Firework.class);
+        FireworkMeta meta = firework.getFireworkMeta();
+
+        FireworkEffect.Builder builder = FireworkEffect.builder();
+        builder.trail(true).flicker(true);
+        builder.withColor(color);
+        builder.with(type);
+
+        meta.addEffect(builder.build());
+        meta.setPower(1);
+        firework.setFireworkMeta(meta);
+
+        return firework;
+    }
+
+    public static Firework createVictoryFireworkStar(World world, Location location){
+        return createVictoryFirework(world, location, Color.SILVER, FireworkEffect.Type.STAR);
+    }
+
+    public static Firework createVictoryFireworkBurst(World world, Location location){
+        return createVictoryFirework(world, location, Color.RED, FireworkEffect.Type.BURST);
+    }
+
+    public static Firework createVictoryFireworkCreeper(World world, Location location){
+        return createVictoryFirework(world, location, Color.GREEN, FireworkEffect.Type.CREEPER);
+    }
+
+    public static Firework createVictoryFireworkBall(World world, Location location){
+       return createVictoryFirework(world, location, Color.BLUE, FireworkEffect.Type.BALL);
     }
 }
