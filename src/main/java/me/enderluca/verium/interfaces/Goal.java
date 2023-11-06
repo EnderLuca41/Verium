b95faa91b1258ba39b5452b46eba816e15119ec4
EnderLuca41 <lucavogel111@gmail.com>
2023-11-06 22:40:12 +0100

    Added interface for goals to implement

---
+++ src/main/java/me/enderluca/verium/interfaces/Goal.java
@@ -0,0 +1,55 @@
+package me.enderluca.verium.interfaces;
+
+import me.enderluca.verium.GoalType;
+import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import javax.annotation.Nullable;
+
+/**
+ * Common interface for all Goal services to implement
+ */
+public interface Goal {
+
+    boolean isCompleted();
+
+    /**
+     * Resets the goal completed status
+     */
+    void reset();
+
+    /**
+     * Returns the message to be broadcast to all players when the goal is complete
+     */
+    @Nullable
+    BaseComponent[] getCompleteMessage();
+
+
+    void setEnabled(boolean val);
+
+    boolean isEnabled();
+
+
+    void setPaused(boolean val);
+
+    boolean isPaused();
+
+    /**
+     * Loads all for the goal necessary information from {@code src}
+     */
+    void loadConfig(FileConfiguration src);
+
+    /**
+     * Saves all for the goal necessary information to  {@code dest} <br>
+     * This includes world dependent infos
+     */
+    void saveConfig(FileConfiguration dest);
+
+    /**
+     * Cleans the config from world specific information
+     */
+    void cleanWoldSpecificConfig(FileConfiguration dest);
+
+
+    GoalType getType();
+}
