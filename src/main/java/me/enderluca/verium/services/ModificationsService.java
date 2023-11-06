4a1d41b4fb622d3c08c1cda02699d93a863ddd3e
EnderLuca41 <lucavogel111@gmail.com>
2023-11-06 22:42:39 +0100

    Added goal serivce to ModificationsService

--- src/main/java/me/enderluca/verium/services/ModificationsService.java
+++ src/main/java/me/enderluca/verium/services/ModificationsService.java
@@ -1,14 +1,21 @@
 package me.enderluca.verium.services;
 
+import me.enderluca.verium.util.EntityUtil;
+import me.enderluca.verium.util.MessageUtil;
 import net.md_5.bungee.api.chat.BaseComponent;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 //TODO: Find better name for this class
 
@@ -19,13 +26,18 @@ public class ModificationsService {
 
     private final ChallengesService challenges;
     private final GamerulesService gamerules;
+    private final GoalsService goals;
 
     private final TimerService timer;
+
+    private final Plugin owner;
 
     public ModificationsService(Plugin owner, FileConfiguration fileConfig, TimerService timer){
         this.gamerules = new GamerulesService(owner, fileConfig);
         this.challenges = new ChallengesService(owner, fileConfig, this::onChallengeFail);
+        this.goals = new GoalsService(owner, fileConfig, this::onAllGoalsComplete);
         this.timer = timer;
+        this.owner = owner;
     }
 
     private void onChallengeFail(@Nullable BaseComponent[] message){
@@ -33,6 +45,39 @@ public class ModificationsService {
 
         if(message != null)
             Bukkit.spigot().broadcast(message);
+    }
+
+    private void onAllGoalsComplete(){
+        pause();
+
+        Long timerSeconds = null;
+        if(timer.isEnabled())
+            timerSeconds = timer.getEplSeconds();
+
+        Bukkit.spigot().broadcast(MessageUtil.buildAllGoalsComplete(timerSeconds));
+
+        //Relative positions from the player where fireworks should appear
+        Vector pos1 = new Vector(0, 0, 0);
+        Vector pos2 = new Vector(1, 0, 0);
+        Vector pos3 = new Vector(0, 0 , 1);
+        Vector pos4 = new Vector(1, 0, 1);
+
+        List<Firework> fireworks = new ArrayList<>();
+        for(Player p : Bukkit.getOnlinePlayers()){
+            fireworks.add(EntityUtil.createVictoryFireworkStar(p.getWorld(), p.getLocation().add(pos1)));
+            fireworks.add(EntityUtil.createVictoryFireworkBurst(p.getWorld(), p.getLocation().add(pos2)));
+            fireworks.add(EntityUtil.createVictoryFireworkCreeper(p.getWorld(), p.getLocation().add(pos3)));
+            fireworks.add(EntityUtil.createVictoryFireworkBall(p.getWorld(), p.getLocation().add(pos4)));
+        }
+
+        BukkitRunnable detonateFireworks = new BukkitRunnable() {
+            @Override
+            public void run() {
+                fireworks.forEach(Firework::detonate);
+            }
+        };
+
+        detonateFireworks.runTaskLater(owner, 40);
     }
 
     /**
@@ -44,6 +89,7 @@ public class ModificationsService {
 
         challenges.setPausedAll(true);
         gamerules.setPausedAll(true);
+        goals.setPausedAll(true);
 
         for(Player p : Bukkit.getOnlinePlayers()){
             p.setGameMode(GameMode.SPECTATOR);
@@ -63,6 +109,7 @@ public class ModificationsService {
 
         gamerules.setPausedAll(false);
         challenges.setPausedAll(false);
+        goals.setPausedAll(false);
 
         timer.start();
     }
