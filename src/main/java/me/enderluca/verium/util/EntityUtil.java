eeac76a2db20314982925ae5d3b729de76787dc1
EnderLuca41 <lucavogel111@gmail.com>
2023-12-14 18:49:18 +0100

    Added javadoc comments

--- src/main/java/me/enderluca/verium/util/EntityUtil.java
+++ src/main/java/me/enderluca/verium/util/EntityUtil.java
@@ -24,6 +24,9 @@ public final class EntityUtil {
         return wolf;
     }
 
+    /**
+     * Template for all victory fireworks
+     */
     private static Firework createVictoryFirework(World world, Location location, Color color, FireworkEffect.Type type){
         Firework firework = world.spawn(location, Firework.class);
         FireworkMeta meta = firework.getFireworkMeta();
@@ -40,18 +43,30 @@ public final class EntityUtil {
         return firework;
     }
 
+    /**
+     * Spawn a victory firework of effect type star
+     */
     public static Firework createVictoryFireworkStar(World world, Location location){
         return createVictoryFirework(world, location, Color.SILVER, FireworkEffect.Type.STAR);
     }
 
+    /**
+     * Spawn a victory firework of effect type burst
+     */
     public static Firework createVictoryFireworkBurst(World world, Location location){
         return createVictoryFirework(world, location, Color.RED, FireworkEffect.Type.BURST);
     }
 
+    /**
+     * Spawn a victory firework of effect type creeper
+     */
     public static Firework createVictoryFireworkCreeper(World world, Location location){
         return createVictoryFirework(world, location, Color.GREEN, FireworkEffect.Type.CREEPER);
     }
 
+    /**
+     * Spawn a victory firework of effect type ball
+     */
     public static Firework createVictoryFireworkBall(World world, Location location){
        return createVictoryFirework(world, location, Color.BLUE, FireworkEffect.Type.BALL);
     }
