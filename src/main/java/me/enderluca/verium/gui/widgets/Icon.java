2646d294382132a17f90392d41a62215c47b4468
EnderLuca41 <lucavogel111@gmail.com>
2024-10-30 21:36:07 +0100

    Add some documentation to the icon class

--- src/main/java/me/enderluca/verium/gui/widgets/Icon.java
+++ src/main/java/me/enderluca/verium/gui/widgets/Icon.java
@@ -3,6 +3,9 @@ package me.enderluca.verium.gui.widgets;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+/**
+ * A simple widget that displays an itemstack that is not clickable
+ */
 public class Icon extends Widget {
     public Icon(ItemStack icon){
         this.icon = icon;
