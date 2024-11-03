347efead0685050c1b3a539d64c201b4908bea77
EnderLuca41 <lucavogel111@gmail.com>
2024-11-03 01:04:55 +0100

    Structured imports

--- src/main/java/me/enderluca/verium/services/goals/KillEnderdragonGoal.java
+++ src/main/java/me/enderluca/verium/services/goals/KillEnderdragonGoal.java
@@ -3,11 +3,11 @@ package me.enderluca.verium.services.goals;
 import me.enderluca.verium.GoalType;
 import me.enderluca.verium.interfaces.Goal;
 import me.enderluca.verium.listener.KillEnderdragonListener;
-import me.enderluca.verium.util.MessageUtil;
+
 import net.md_5.bungee.api.chat.BaseComponent;
+
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import javax.annotation.Nullable;
