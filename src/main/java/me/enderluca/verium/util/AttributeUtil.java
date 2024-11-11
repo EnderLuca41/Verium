ebde87dfdfaa2cea66a3b7127f0dbfbb8527b04f
EnderLuca41 <lucavogel111@gmail.com>
2024-11-11 18:25:34 +0100

    Changed attribute util to use the new names of attributes and replaced the switch statements with hashmaps because the new attribute implementation is not an enum anymore.

--- src/main/java/me/enderluca/verium/util/AttributeUtil.java
+++ src/main/java/me/enderluca/verium/util/AttributeUtil.java
@@ -2,6 +2,9 @@ package me.enderluca.verium.util;
 
 import org.bukkit.attribute.Attribute;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class AttributeUtil {
     private AttributeUtil(){}
 
@@ -9,70 +12,78 @@ public final class AttributeUtil {
      * Array containing all attributes that apply to a player
      */
     public static final Attribute[] PLAYER_ATTRIBUTES = {
-            Attribute.GENERIC_ARMOR, Attribute.GENERIC_ARMOR_TOUGHNESS,
-            Attribute.GENERIC_ATTACK_DAMAGE, Attribute.GENERIC_ATTACK_SPEED,
-            Attribute.GENERIC_KNOCKBACK_RESISTANCE, Attribute.GENERIC_LUCK,
-            Attribute.GENERIC_MAX_HEALTH, Attribute.GENERIC_MOVEMENT_SPEED
+            Attribute.ARMOR, Attribute.ARMOR_TOUGHNESS,
+            Attribute.ATTACK_DAMAGE, Attribute.ATTACK_SPEED,
+            Attribute.KNOCKBACK_RESISTANCE, Attribute.LUCK,
+            Attribute.MAX_HEALTH, Attribute.MOVEMENT_SPEED
     };
+
+    private static final Map<Attribute, Double> defaultBaseValues = new HashMap<>();
+
+    static {
+        defaultBaseValues.put(Attribute.ARMOR, 0.0);
+        defaultBaseValues.put(Attribute.ARMOR_TOUGHNESS, 0.0);
+        defaultBaseValues.put(Attribute.ATTACK_DAMAGE, 1.0);
+        defaultBaseValues.put(Attribute.ATTACK_SPEED, 4.0);
+        defaultBaseValues.put(Attribute.KNOCKBACK_RESISTANCE, 0.0);
+        defaultBaseValues.put(Attribute.LUCK, 0.0);
+        defaultBaseValues.put(Attribute.MAX_HEALTH, 20.0);
+        defaultBaseValues.put(Attribute.MOVEMENT_SPEED, 0.1);
+    }
 
     /**
      * Returns the default attribute base value for a player
      */
     public static double getDefaultBaseValue(Attribute attribute){
-        return switch (attribute){
-            case GENERIC_ARMOR -> 0.0;
-            case GENERIC_ARMOR_TOUGHNESS -> 0.0;
-            case GENERIC_ATTACK_DAMAGE -> 1.0;
-            case GENERIC_ATTACK_SPEED -> 4.0;
-            case GENERIC_KNOCKBACK_RESISTANCE -> 0.0;
-            case GENERIC_LUCK -> 0.0;
-            case GENERIC_MAX_HEALTH -> 20.0;
-            case GENERIC_MOVEMENT_SPEED -> 0.1;
-            default -> 0.0; //If attribute is not applicable to player, return 0.0
-            //NOTE: attribute max absorption was added in 1.20.2
-            //NOTE 2: 1.20.5 Also adds 4 new attributes
-        };
+        return defaultBaseValues.getOrDefault(attribute, 0.0);
+    }
+
+    private static final Map<Attribute, Double> minValues = new HashMap<>();
+
+    static {
+        minValues.put(Attribute.ARMOR, 0.0);
+        minValues.put(Attribute.ARMOR_TOUGHNESS, 0.0);
+        minValues.put(Attribute.ATTACK_DAMAGE, 0.0);
+        minValues.put(Attribute.ATTACK_SPEED, 0.0);
+        minValues.put(Attribute.FLYING_SPEED, 0.0);
+        minValues.put(Attribute.FOLLOW_RANGE, 0.0);
+        minValues.put(Attribute.KNOCKBACK_RESISTANCE, 0.0);
+        minValues.put(Attribute.LUCK, -1024.0);
+        minValues.put(Attribute.MAX_HEALTH, 1.0);
+        minValues.put(Attribute.MOVEMENT_SPEED, 0.0);
+        minValues.put(Attribute.JUMP_STRENGTH, 0.0);
+        minValues.put(Attribute.SPAWN_REINFORCEMENTS, 0.0);
     }
 
     /**
      * Returns the minimum value of a given attribute
      */
     public static double getMinValue(Attribute attribute){
-        return switch(attribute){
-            case GENERIC_ARMOR -> 0.0;
-            case GENERIC_ARMOR_TOUGHNESS -> 0.0;
-            case GENERIC_ATTACK_DAMAGE -> 0.0;
-            case GENERIC_ATTACK_KNOCKBACK -> 0.0;
-            case GENERIC_ATTACK_SPEED -> 0.0;
-            case GENERIC_FLYING_SPEED -> 0.0;
-            case GENERIC_FOLLOW_RANGE -> 0.0;
-            case GENERIC_KNOCKBACK_RESISTANCE -> 0.0;
-            case GENERIC_LUCK -> -1024.0;
-            case GENERIC_MAX_HEALTH -> 1.0;
-            case GENERIC_MOVEMENT_SPEED -> 0.0;
-            case HORSE_JUMP_STRENGTH -> 0.0;
-            case ZOMBIE_SPAWN_REINFORCEMENTS -> 0.0;
-        };
+        return minValues.get(attribute);
     }
 
+    private static final Map<Attribute, Double> maxValues = new HashMap<>();
+
+    static {
+        maxValues.put(Attribute.ARMOR, 30.0);
+        maxValues.put(Attribute.ARMOR_TOUGHNESS, 20.0);
+        maxValues.put(Attribute.ATTACK_DAMAGE, 2048.0);
+        maxValues.put(Attribute.ATTACK_KNOCKBACK, 5.0);
+        maxValues.put(Attribute.ATTACK_SPEED, 1024.0);
+        maxValues.put(Attribute.FLYING_SPEED, 1024.0);
+        maxValues.put(Attribute.FOLLOW_RANGE, 2048.0);
+        maxValues.put(Attribute.KNOCKBACK_RESISTANCE, 1.0);
+        maxValues.put(Attribute.LUCK, 1024.0);
+        maxValues.put(Attribute.MAX_HEALTH, 1024.0);
+        maxValues.put(Attribute.MOVEMENT_SPEED, 1024.0);
+        maxValues.put(Attribute.JUMP_STRENGTH, 32.0);
+        maxValues.put(Attribute.SPAWN_REINFORCEMENTS, 1.0);
+
+    }
     /**
      * Returns the maximum value of a given attribute
      */
     public static double getMaxValue(Attribute attribute){
-        return switch(attribute){
-            case GENERIC_ARMOR -> 30.0;
-            case GENERIC_ARMOR_TOUGHNESS -> 20.0;
-            case GENERIC_ATTACK_DAMAGE -> 2048.0;
-            case GENERIC_ATTACK_KNOCKBACK -> 5.0;
-            case GENERIC_ATTACK_SPEED -> 1024.0;
-            case GENERIC_FLYING_SPEED -> 1024.0;
-            case GENERIC_FOLLOW_RANGE -> 2048.0;
-            case GENERIC_KNOCKBACK_RESISTANCE -> 1.0;
-            case GENERIC_LUCK -> 1024.0;
-            case GENERIC_MAX_HEALTH -> 1024.0;
-            case GENERIC_MOVEMENT_SPEED -> 1024.0;
-            case HORSE_JUMP_STRENGTH -> 32.0;
-            case ZOMBIE_SPAWN_REINFORCEMENTS -> 1.0;
-        };
+        return maxValues.get(attribute);
     }
 }
