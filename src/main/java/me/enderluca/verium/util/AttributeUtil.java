8d0dc1ea8fb94092e549d361cc66d95e80f1c398
EnderLuca41 <lucavogel111@gmail.com>
2024-11-14 19:46:58 +0100

    Created new readonly set to give every attribute an id, this id will be used for saving attributes and restoring them from memory

--- src/main/java/me/enderluca/verium/util/AttributeUtil.java
+++ src/main/java/me/enderluca/verium/util/AttributeUtil.java
@@ -2,6 +2,7 @@ package me.enderluca.verium.util;
 
 import org.bukkit.attribute.Attribute;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,6 +28,48 @@ public final class AttributeUtil {
             Attribute.SUBMERGED_MINING_SPEED, Attribute.SWEEPING_DAMAGE_RATIO,
             Attribute.WATER_MOVEMENT_EFFICIENCY
     };
+
+    //Maps an id to every attribute available, to save and load from memory
+    public static final Map<Attribute, Integer> ATTRIBUTE_IDS;
+
+    static {
+        Map<Attribute, Integer> set = new HashMap<>();
+        set.put(Attribute.ARMOR, 0);
+        set.put(Attribute.ARMOR_TOUGHNESS, 1);
+        set.put(Attribute.ATTACK_DAMAGE, 2);
+        set.put(Attribute.ATTACK_KNOCKBACK, 3);
+        set.put(Attribute.ATTACK_SPEED, 4);
+        set.put(Attribute.FLYING_SPEED, 5);
+        set.put(Attribute.FOLLOW_RANGE, 6);
+        set.put(Attribute.KNOCKBACK_RESISTANCE, 7);
+        set.put(Attribute.LUCK, 8);
+        set.put(Attribute.MAX_ABSORPTION, 9);
+        set.put(Attribute.MAX_HEALTH, 10);
+        set.put(Attribute.MOVEMENT_SPEED, 11);
+        set.put(Attribute.SCALE, 12);
+        set.put(Attribute.STEP_HEIGHT, 13);
+        set.put(Attribute.JUMP_STRENGTH, 14);
+        set.put(Attribute.BLOCK_INTERACTION_RANGE, 15);
+        set.put(Attribute.ENTITY_INTERACTION_RANGE, 16);
+        set.put(Attribute.SPAWN_REINFORCEMENTS, 17);
+        set.put(Attribute.BLOCK_BREAK_SPEED, 18);
+        set.put(Attribute.GRAVITY, 19);
+        set.put(Attribute.SAFE_FALL_DISTANCE, 20);
+        set.put(Attribute.FALL_DAMAGE_MULTIPLIER, 21);
+        set.put(Attribute.BURNING_TIME, 22);
+        set.put(Attribute.EXPLOSION_KNOCKBACK_RESISTANCE, 23);
+        set.put(Attribute.MINING_EFFICIENCY, 24);
+        set.put(Attribute.MOVEMENT_EFFICIENCY, 25);
+        set.put(Attribute.OXYGEN_BONUS, 26);
+        set.put(Attribute.SNEAKING_SPEED, 27);
+        set.put(Attribute.SUBMERGED_MINING_SPEED, 28);
+        set.put(Attribute.SWEEPING_DAMAGE_RATIO, 29);
+        set.put(Attribute.TEMPT_RANGE, 30);
+        set.put(Attribute.WATER_MOVEMENT_EFFICIENCY, 31);
+        ATTRIBUTE_IDS = Collections.unmodifiableMap(set);
+    }
+
+
 
     private static final Map<Attribute, Double> defaultBaseValues = new HashMap<>();
 
