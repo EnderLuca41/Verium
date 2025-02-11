233cb44afb1592dcd4239432f02e2522dee2fa2b
EnderLuca41 <lucavogel111@gmail.com>
2025-02-11 21:50:42 +0100

    Removed not used imports

--- src/main/java/me/enderluca/verium/listener/ModificationsListener.java
+++ src/main/java/me/enderluca/verium/listener/ModificationsListener.java
@@ -1,15 +1,12 @@
 package me.enderluca.verium.listener;
 
 import org.bukkit.GameMode;
-import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.world.WorldLoadEvent;
 
 import java.util.function.BooleanSupplier;
-import java.util.function.Consumer;
 
 public class ModificationsListener implements Listener {
 
